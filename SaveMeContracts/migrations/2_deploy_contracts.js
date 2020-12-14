11d011f89bb63e295aa663ba8e6ccf4bb905075f
cantonioupao <christos0598pao13@gmail.com>
2020-12-14 21:16:50 +0200

    Update to deploy all Solidity Contracts

--- SaveMeContracts/migrations/2_deploy_contracts.js
+++ SaveMeContracts/migrations/2_deploy_contracts.js
@@ -1,7 +1,19 @@
 var SaveMeToken = artifacts.require("./SaveMeToken.sol");
 var TokenValidation = artifacts.require("./TokenValidation.sol");
+var HealthInsurance = artifacts.require('.HealthInsurance.sol');
+var DataAccess = artifacts.require('./DataAccess.sol');
+var IPFS = artifacts.require('./IPFS.sol');
 var _initialSupply = 0 ;
 
 module.exports = function(deployer) {
   deployer.deploy( SaveMeToken , _initialSupply ); // the first is the initial supply, then the initial_supplyOwner and the tokenCreator
+
+  deployer.deploy(TokenValidation);
+
+  deployer.deploy(HealthInsurance);
+
+  deployer.deploy(DataAccess) ;
+
+  deployer.deploy(IPFS);
+
 };
