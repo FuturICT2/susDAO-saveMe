592ee1f82c1f9db650876fd5101fd045f672b633
cantonioupao <christos0598pao13@gmail.com>
2020-12-14 19:55:12 +0200

    Added address checking for exsting patients

--- SaveMeContracts/contracts/TokenValidation.sol
+++ SaveMeContracts/contracts/TokenValidation.sol
@@ -3,6 +3,16 @@ pragma solidity ^0.5.16;
 import 'contracts/SaveMeToken.sol';
 
 /*This contract creates the tokens based on the IPFS token file retrieved & read */
+//Hence :
+    //We set supply of tokens to 0;
+    //Go to the IPFS file of the account specified by tokenOwner
+    //Retrieve and read the Token IPFS file
+    //Count # of tokens
+    // supply = IPFS_tokens() ; Set supply to this number of tokens
+    //The # of SaveTokens to be created are set by supply
+    //Create an instance of SaveMe token
+    //SaveMeToken token_instance = new SaveMeToken (supply, TokenOwner ,TokenValidationAddress);
+    //SaveMeToken constructor receives (_initialSupply, _initialSupplyOwner, _tokenCreator)
 
 contract TokenValidation is SaveMeToken
 {
@@ -12,6 +22,15 @@ contract TokenValidation is SaveMeToken
     uint256 private supply;
     uint public tokenCreationTime;
     bool private initDone = false;
+    bool private validPatient = false;
+    string private IPFShash ;
+    string private IPNSLink ;
+
+
+    //It maps an address to an IPNS String link. And then this is mapped to an IPFS hash, corresponding to the token IPFS file
+    mapping(address =>mapping(string => string)) private IPFS_tokenHash;
+    mapping(address => string) IPNS_tokenLink ;
+    mapping(address => bool) patients;
 
 
     constructor () public {
@@ -20,26 +39,46 @@ contract TokenValidation is SaveMeToken
 
 
 
-     function init(address _ValidationCaller, address _TokenOwner, uint256 _supply) public {
+     function init(address _ValidationCaller, address _TokenOwner) public {
         require(!initDone, "init() can only be called once"); // TODO also require token creator?
         ValidationCaller = _ValidationCaller;
         TokenOwner = _TokenOwner;
-        supply = _supply;
         initDone = true;
         }
 
-    function deployTokens() public returns (bool success) {
-    TokenValidationAddress == msg.sender; // we want the token creator to be the Token Validation Contract address
-    supply = 0; //set supply of tokens to 0;
-    //Go to the IPFS file of the account specified by tokenOwner
-    //Retrieve and read the Token IPFS file
-    //Count # of tokens
-    // supply = IPFS_tokens() ; Set supply to this number of tokens
-    //The # of SaveTokens to be created are set by supply
-    //Create an instance of SaveMe token
-    //SaveMeToken token_instance = new SaveMeToken (supply, TokenOwner ,TokenValidationAddress);
-    //SaveMeToken constructor receives (_initialSupply, _initialSupplyOwner, _tokenCreator)
-    return true;
+
+    //returns the Hash of the IPFS toke file corresponding to the patient
+    function getIPFSTokensHash() private returns (bool success){
+        require (validPatient, "This Address does not correspond to an existing patient");
+        //go to IPNS link and get the hash
+        IPFShash = IPFS_tokenHash[TokenOwner][IPNS_tokenLink[TokenOwner]];
+        return true;
+    }
+
+
+    function checkAddress(address _to) private returns (bool){
+        
+        validPatient = patients[_to];
+        return true;
+
+    }
+
+    function getValidatedTokens() private returns (uint256){
+        require (validPatient ,"This Address does not correspond to an existing patient");
+        //count tokens inside the IPFS hash
+        //supply = download_and_count(IPFShash) ;
+        return supply ;
+
+    }
+
+
+    function deployTokens( address _from, address  _to) public returns (bool) {
+        supply = 0;
+        init(_from, _to);
+        checkAddress(_to);
+        getIPFSTokensHash();
+        getValidatedTokens();
+        return true ;
     }
 
 }
