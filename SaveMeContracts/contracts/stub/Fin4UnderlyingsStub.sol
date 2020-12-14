fdd11da73c8ca8e1476e0b991c1d8816fa566461
cantonioupao <christos0598pao13@gmail.com>
2020-12-14 04:18:35 +0200

    Got rid of redundant Smart Contracts

--- SaveMeContracts/contracts/stub/Fin4UnderlyingsStub.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.17;
-
-contract Fin4UnderlyingsStub {
-
-    function newSourcererPairAllowedWithPat(address user, address pat) public returns(bool);
-
-    function newSourcererPairAllowedWithCollateral(address user, address pat, address collateral) public returns(bool);
-
-}
